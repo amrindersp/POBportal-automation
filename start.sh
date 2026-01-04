a7c0d51224ed6c289d897baadf1f718e8e8f4e3e
Amrinder Singh <amrindersingh@Amrinders-MacBook-Air.local>
2026-01-04 21:27:29 +0530

    Use python3 in start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Start the RQ worker in the background
-python worker.py &
+python3 worker.py &
 # Start the FastAPI web server in the foreground
-python -m uvicorn app.main:app --host 0.0.0.0 --port $PORT
+python3 -m uvicorn app.main:app --host 0.0.0.0 --port $PORT
