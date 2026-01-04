a4a0f234db19c8158437e492f4afaf291e8ff8b2
Amrinder Singh <amrindersingh@Amrinders-MacBook-Air.local>
2026-01-04 21:10:48 +0530

    Fix uvicorn command in start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,4 @@
 # Start the RQ worker in the background
 python worker.py &
 # Start the FastAPI web server in the foreground
-uvicorn app.main:app --host 0.0.0.0 --port $PORT
-
+python -m uvicorn app.main:app --host 0.0.0.0 --port $PORT
